a6185c3d20988c545c9f65be46074ff1002456a3
Brandon Ellis <ellis.brandon.w@gmail.com>
2023-09-21 18:27:26 -0400

    added getComputerChoice function

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 
+function getComputerChoice() {
+    let compChoice = Math.random();
+    if (compChoice <= 0.32) {
+        return "Rock!";
+    } else if (compChoice > 0.32 && compChoice <= 0.65) {
+        return "Paper!";
+    } else return "Scissors!"
+};
+console.log(getComputerChoice())
