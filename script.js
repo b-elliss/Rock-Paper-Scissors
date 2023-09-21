2b1e9e0875f0c0ce43dd9d9df2b29e03500066dd
Brandon Ellis <ellis.brandon.w@gmail.com>
2023-09-21 19:41:58 -0400

    playRound function added. Seem unable to return a tie

--- script.js
+++ script.js
@@ -2,9 +2,23 @@
 function getComputerChoice() {
     let compChoice = Math.random();
     if (compChoice <= 0.32) {
-        return "Rock!";
+        return "Rock";
     } else if (compChoice > 0.32 && compChoice <= 0.65) {
-        return "Paper!";
-    } else return "Scissors!"
+        return "Paper";
+    } else return "Scissors"
 };
-console.log(getComputerChoice())
+/*console.log(getComputerChoice())*/
+function playRound(playerSelection, computerSelection) {
+    if (playerSelection == computerSelection) {
+        return "Tie! Play again";
+    } else if (playerSelection == 'rock' && computerSelection == 'Paper') {
+        return "You lose! Paper beats Rock.";
+    } else if (playerSelection == 'paper' && computerSelection == 'Scissors') {
+        return "You lose! Scissors beats paper.";
+    } else if (playerSelection == 'scissors' && computerSelection == 'Rock') {
+        return "You lose! Rock beats scissors.";
+    } else return "You win!"
+};
+const playerSelection = "paper";
+const computerSelection = getComputerChoice();
+console.log(playRound(playerSelection, computerSelection));
