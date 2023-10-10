fdacfb4103bf486b860fd914bb778c6fa5940a13
Brandon Ellis <ellis.brandon.w@gmail.com>
2023-10-09 20:31:49 -0400

    app is functional but needs reset feature

--- script.js
+++ script.js
@@ -1,40 +1,55 @@
-
-function getComputerChoice() {
-    let compChoice = Math.floor(Math.random() * 3);
-    switch (compChoice) {
-        case 0:
-            return('rock');
-            break;
-        case 1:
-            return('paper');
-            break;
-        case 2:
-            return('scissors');
-            break;
-    }
+let playerScore = 0;
+let computerScore = 0;
+const gameButtons = document.querySelectorAll('.gameButton');
+let compChoice;
+const compArr = ["rock","paper","scissors"];
+const getCompChoice = () => {
+    compChoice = compArr[Math.floor(Math.random() * 3)];
 };
 
-const compChoice = getComputerChoice();
+gameButtons.forEach(gameButtons => {
+    gameButtons.addEventListener('click', () => {
+        getCompChoice();
+        let playerButton = gameButtons.textContent.toLowerCase();
+        document.querySelector('#gamecomments').textContent = `${playRound(playerButton, compChoice)}`;
+        document.querySelector('.computerChoice').textContent = `Computer chose ${compChoice}!`;
+        document.querySelector('.playerChoice').textContent = `You chose ${playerButton}!`;
+        document.querySelector('#playerScore').textContent = `${playerScore}`;
+        document.querySelector('#computerScore').textContent = `${computerScore}`;
+        if (playerScore == '5') {
+            return alert("You won the game!");
+        } else if (computerScore == '5') {
+            return alert("You lost to the computer :'(")
+        };
+    });
+});
 
-function playRound(playerSelection, computerSelection) {
-    //let playerSelection = prompt("Rock, Paper or Scissors?");
+const playRound = (playerSelection, computerSelection) => {
     if (playerSelection == computerSelection) {
-        return alert("Tie! Play again");
+        return "Tie! Play again";
     } else if (playerSelection == 'rock' && computerSelection == 'paper') {
-        return alert("You lose! Paper beats Rock.");
+        computerScore++;
+        return "You lose! Paper beats Rock."; 
     } else if (playerSelection == 'paper' && computerSelection == 'scissors') {
-        return alert("You lose! Scissors beats paper.");
+        computerScore++;
+        return "You lose! Scissors beats paper.";
     } else if (playerSelection == 'scissors' && computerSelection == 'rock') {
-        return alert("You lose! Rock beats scissors.");
-    } else return alert("You win!")
-};
-
-function game() {
-    for(i = 0; i < 5; i++) {
-        console.log(playRound('rock', compChoice));
+        computerScore++;
+        return "You lose! Rock beats scissors."; 
+    } else if (playerSelection == 'rock' && computerSelection == 'scissors') {
+        playerScore++;
+        return "You win! Rock beats scissors.";
+    } else if (playerSelection == 'paper' && computerSelection == 'rock') {
+        playerScore++;
+        return "You win! Paper beats rock.";
+    } else if (playerSelection == 'scissors' && computerSelection == 'paper') {
+        playerScore++;
+        return "You win! Scissors beats paper.";
     }
 };
 
-console.log(playRound('rock', compChoice));
-console.log(compChoice);
+console.log(playerScore);
+console.log(computerScore);
+
+//console.log(compChoice);
 //console.log(game());
